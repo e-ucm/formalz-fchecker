13b0d7a10732f8a11a9f2fcb195d0466bffcbc54
Ogilvie, D.H. (Duncan) <d.h.ogilvie2@students.uu.nl>
2017-09-08 14:37:40 +0200

    fix operator precedence in language-java (closes #1)

--- examples/DuncanExample.java
+++ examples/DuncanExample.java
@@ -3,13 +3,24 @@ public class DuncanExample {
     boolean f1(int x, int y) {
         return !(x>0) && (x > -2) ;
     }
+
+    boolean f1_(int x, int y) {
+        return !(x>0) && x > -2;
+    }
     
     boolean f2(int x, int y) {
         return !(x>0) && (x > 0) ;
+    }
+
+    boolean f2_(int x, int y) {
+        return !(x>0) && x > 0 ;
     }
     
     boolean f3(int[] a, int k) {
         return (k>9) && (a[k]>0) && (a[k] == a[k+1]) ;
     }
     
+    boolean f3_(int[] a, int k) {
+        return k>9 && a[k]>0 && a[k] == a[k+1] ;
+    }
 }
