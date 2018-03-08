3a1e29be04aecd1ec0e6126568f7d6e4891c81b6
Joris ten Tusscher <joristt@gmail.com>
2018-03-08 15:00:33 +0100

    Fixed bug in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@
 
 PORT="${1:-8080}"
 LOG="${2:-out.log}"
+LOGPATH="$(pwd)/$LOG"
+BACKUPPATH="$(pwd)/$LOG".backup
 
 # Get latest HEAD
 git pull origin master
@@ -12,6 +14,10 @@ stack build
 # Kill previous process
 pkill javawlp
 # Backup previous log if it exists by appending it to a backup file.
-[[ -f $LOG ]] && cat $LOG >> "$LOG".backup
+if [[ -f "$LOGPATH" ]]
+then
+	cat "$LOGPATH" >> "$BACKUPPATH"
+	rm "$LOGPATH"
+fi
 # Run server
 nohup stack exec javawlp -- --runServer -p $PORT > $LOG &
